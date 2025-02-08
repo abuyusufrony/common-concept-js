fc304616cedd868f293bdee81672520a648f6030
abuyusufrony <abuyusufrony@gmail.com>
2025-02-08 13:06:54 +0600

    true-false

---
+++ true-false.js
@@ -0,0 +1,7 @@
+const x = [];
+if (x) {
+    console.log('x is truely');
+}
+else {
+    console.log('x is false')
+}
---
+++ undifine.js
@@ -0,0 +1,11 @@
+let first;
+console.log(first)
+
+function undifine(a, b) {
+    const to = a + b;
+    console.log(a, b)
+
+}
+undifine(1,)
+const given = undifine(5,)
+
