186e84ffc6d1797a0cbd7c496f39d676142fdb87
abuyusufrony <abuyusufrony@gmail.com>
2025-02-08 20:46:14 +0600

    passBy-value

--- passBy.js
+++ passBy.js
@@ -2,7 +2,8 @@ let couple1 = { name: 'ananata', partner: 'Borhsa' }
 let couple2 = { name: 'raj', partner: 'porimoni' }
 function makeMovie(movie1, movie2) {
     couple1.name = 'rony';
+    couple2.name = 'bony'
 }
 console.log(couple1)
 makeMovie(couple1, couple2)
-console.log(couple1)
+console.log(couple1, couple2)
